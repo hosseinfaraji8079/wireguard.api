2846086472ab73bd598a490990eb209b937c003e
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-03 20:07:42 +0330

    fixed wg show

--- wireguard.api/generatefile.cs
+++ wireguard.api/generatefile.cs
@@ -16,15 +16,11 @@ namespace wireguard.api
                           PreDown = 
                           PostDown = 
                           ListenPort = 
-                          PrivateKey = AAGh98dWA4t54t32Y+4Fw/KGI3XyQ9XLzVYreDIQAm0=
+                          PrivateKey = AAGh98dWA4t54t32Y+4Fw/KGI3XyQ9XLxVYreDIQAm0=
 
-                          [Peer]
-                          PublicKey = EPa3qSY/Xobd5uobkNGa15ot/4e35/MJO4ESaeWRWF4=
-                          PresharedKey = {Guid.NewGuid().ToString("N")}
-                          AllowedIPs = 10.0.0.2/24
                           """;
 
-            await using (StreamWriter writer = new StreamWriter("/etc/wireguard/test.conf", append: false))
+            await using (StreamWriter writer = new StreamWriter("/etc/wireguard/test2.conf", append: false))
             {
                 await writer.WriteLineAsync(content);
             }
